68cfb6beeb82114a3d7878d9485e6e24208f57db
thales-pinner <thales.neves@pinpeople.com.br>
2023-10-02 21:57:50 -0300

    Add script for docker compose

---
+++ setup_app.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+set -e
+
+# yarn install
+
+bundle check || bundle install --jobs 5 --retry 5
+
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
+
+
+if [ "$RAILS_ENV" != development]; then
+  echo "Running migrations"
+  rails db:migrate
+  echo "End of migrations"
+else
+  echo "Skipping migrations for Dev Env"
+fi
+
+
+exec "$@" # command from docker-compose
