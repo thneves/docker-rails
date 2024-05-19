901417ff57e55a5990b9ec086a101d904071d96c
thales-pinner <thales.neves@pinpeople.com.br>
2024-05-19 18:11:15 -0300

    add bootstrap and configs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,22 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "trix"
+import "@rails/actiontext"
+import Rails from "@rails/ujs"
+import "popper"
+import "./controllers"
+import * as bootstrap from "bootstrap"
+
+Rails.start()
+
+
+import { Dropdown } from 'bootstrap'
+
+document.addEventListener('turbo:load', () => {
+  // Initialize all dropdowns
+  var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+  var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+    return new Dropdown(dropdownToggleEl)
+  })
+})
